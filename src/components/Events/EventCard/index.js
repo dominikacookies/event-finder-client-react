c765b3a0b5b074afb6820761ea95b45cfc72f4df
Dominika <domi_nika@hotmail.co.uk>
2021-07-20 16:06:14 +0100

    addec correct values to cards

--- src/components/Events/EventCard/index.js
+++ src/components/Events/EventCard/index.js
@@ -1,16 +1,14 @@
 const EventCard = ({ event }) => {
-  console.log(event.name);
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img className="card-img-top" src="..." alt="event" />
       <div className="card-body">
         <h5 className="card-title">{event.name}</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <p>Date: </p>
+        <p>Time: </p>
+        <p>Venue: </p>
         <a href="#" className="btn btn-primary">
-          Go somewhere
+          Book now
         </a>
       </div>
     </div>
