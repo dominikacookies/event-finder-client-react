268427a0bc20b3a54b862cc7654b1b31b236ea81
Dominika <domi_nika@hotmail.co.uk>
2021-07-22 21:42:17 +0100

    added route for events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Events from "./pages/Events";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
+          <Route path="/events">
+            <Events />
+          </Route>
           <Route path="/">
             <Homepage />
           </Route>
